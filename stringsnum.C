66bb1a28de474e7354204a80b3edc6457fd1fb06
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 16:08:24 +0530

    prgm

---
+++ stringsnum.C
@@ -0,0 +1,29 @@
+  
+#include<stdio.h>
+#include<conio.h>
+
+#define MAX_STRING_LEN 80
+
+int main() 
+  
+
+  char S1[MAX_STRING_LEN];
+  char S2[MAX_STRING_LEN];
+  
+  int i, l, res;
+
+  printf("String1:\t");
+  scanf("%s",S1);
+
+  printf("String2:\t");
+  scanf("%s",S2);
+  {
+  intr(i=0;i++;i<4);
+
+  res = strcmp(S1,S2);
+
+  printf("strcmp(%sS1,%sS2) = %d\n",S1,S2,res);
+
+}
+getch();
+}
